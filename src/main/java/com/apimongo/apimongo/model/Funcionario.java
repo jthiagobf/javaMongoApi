e654bf8d2ec4abbc58718cb78622070fe3806041
unknown <j.thiagobf1992@gmail.com>
2022-05-22 11:03:14 -0300

    created connection with MongoDB

--- src/main/java/com/apimongo/apimongo/model/Funcionario.java
+++ src/main/java/com/apimongo/apimongo/model/Funcionario.java
@@ -21,6 +21,48 @@ public class Funcionario {
 	private BigDecimal salario;
 	
 	private Funcionario chefe;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public Funcionario getChefe() {
+		return chefe;
+	}
+
+	public void setChefe(Funcionario chefe) {
+		this.chefe = chefe;
+	}
+	
+	
 	
 	
 }
